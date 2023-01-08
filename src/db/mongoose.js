a181b0701e4eb6a404a1a5ceb375a1910c6a3504
Anik Ghosh <ldexter0910@gmail.com>
2023-01-09 01:53:51 +0530

    Error handling added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,4 +3,6 @@ const mongoose = require('mongoose');
 const dbUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-app'
 
-mongoose.connect(`${dbUrl}/${dbName}`);
+mongoose.connect(`${dbUrl}/${dbName}`).catch(e => {
+    console.log(e);
+});
--- src/routers/users.route.js
+++ src/routers/users.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user.model');
+const auth = require('../middleware/auth.middleware');
 
 const router = new express.Router();
 
